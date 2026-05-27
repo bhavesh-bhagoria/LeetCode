aa79402b66e332df41e865c944fe8cabaadcd947
Bhavesh Bhagoria <bhaveshbhagoria940@gmail.com>
2026-05-27 16:12:00 +0530

    Time: 23 ms (7.37%), Space: 21.2 MB (52.15%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,18 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counted = set()
-        x = 0
-        while x < len(nums):
-            element = nums[x]
-            if element in counted:        # ✅ Skip if we already counted this
-                x += 1
-                continue
-            counted.add(element)
-
-            count = 0
-            for key in nums:
-                if key == element:
-                    count += 1
-                    if count > len(nums) / 2:
-                        return element
-            x += 1
+        hash_map = {}
+        count = 1
+        n= len(nums)
+        for f in range(n):
+            if nums[f] not in hash_map:
+                hash_map[nums[f]] = count
+            elif nums[f] in hash_map:
+                hash_map[nums[f]] +=1
+        return max(hash_map, key=hash_map.get)
