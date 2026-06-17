072f64df3c53e53ad478c868c200e53a6a7933cd
Bhavesh Bhagoria <bhaveshbhagoria940@gmail.com>
2026-06-17 16:14:39 +0530

    Time: 480 ms (9.24%), Space: 26.5 MB (70.5%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,15 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        x=0
-        y=n
-        while x<k:
-            y=n
-            item = nums.pop(y-1)
-            nums.insert(0,item)
-            x+=1
-            y+=1
-
-           
-
+        temp = []
+        k = k % len(nums)
+        while k < len(nums):
+            temp.append(nums.pop(0))
+        nums.extend(temp)
