276c6f9028cac9a90897b332c5528da0d263dc0e
Bhavesh Bhagoria <bhaveshbhagoria940@gmail.com>
2026-08-31 22:43:29 +0530

    Time: 11 ms (54.96%), Space: 20.2 MB (64.44%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -2,12 +2,19 @@ class Solution:
     def frequencySort(self, s: str) -> str:
         freq = {}
 
-        for i in range(len(s)):
-            if s[i] not in freq:
-                freq[s[i]] = 1
+        for char in s:
+            if char not in freq:
+                freq[char] = 1
             else:
-                freq[s[i]] += 1
+                freq[char] += 1
 
+        # For s = "tree":
+        # freq = {'t': 1, 'r': 1, 'e': 2}
+        # sorted_char = [('e', 2), ('t', 1), ('r', 1)]
+        #
+        # sorted() takes O(k log k) time because it uses a comparison-based
+        # sorting algorithm (Timsort). Here k = number of unique characters.
+        # Since k <= n (length of s), worst case is O(n log n).
         sorted_char = sorted(freq.items(), key=lambda x: x[1], reverse=True)
 
         final = ""
